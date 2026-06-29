8f70e526c4519539ea70589e48c8ca1ec7d11ad8
mrdiamalick-maker <mrdiamalick@gmail.com>
2026-06-29 17:02:28 +0200

    fix: brancher devis sur le formulaire interactif

--- src/app/devis/page.tsx
+++ src/app/devis/page.tsx
@@ -1,5 +1,5 @@
-import { LeadPage } from "@/components/experience/CleanExperience";
+import { LeadExperience } from "@/components/experience/LeadExperience";
 
 export default function Page() {
-  return <LeadPage kind="devis" />;
+  return <LeadExperience kind="devis" />;
 }
