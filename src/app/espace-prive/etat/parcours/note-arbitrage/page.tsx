fe063a37ca041568534ea2f8041cd5f239f7b415
mrdiamalick-maker <mrdiamalick@gmail.com>
2026-07-04 20:54:12 +0200

    Add note arbitration journey page

---
+++ src/app/espace-prive/etat/parcours/note-arbitrage/page.tsx
@@ -0,0 +1,5 @@
+import { EtatParcoursWorkbench } from "@/components/private-space/EtatParcoursWorkbench";
+
+export default function NoteArbitrageJourneyPage() {
+  return <EtatParcoursWorkbench kind="note-arbitrage" />;
+}
