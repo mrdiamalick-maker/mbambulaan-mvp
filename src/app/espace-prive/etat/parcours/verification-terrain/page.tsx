236f7e9fa3fba4e1a892740bd5ba771f85db76a2
mrdiamalick-maker <mrdiamalick@gmail.com>
2026-07-04 21:05:06 +0200

    Add field verification journey page

---
+++ src/app/espace-prive/etat/parcours/verification-terrain/page.tsx
@@ -0,0 +1,5 @@
+import { EtatParcoursWorkbench } from "@/components/private-space/EtatParcoursWorkbench";
+
+export default function VerificationTerrainJourneyPage() {
+  return <EtatParcoursWorkbench kind="verification-terrain" />;
+}
