4515f9f3c9c709556e44cf335b2b4c4bc2cfd574
mrdiamalick-maker <mrdiamalick@gmail.com>
2026-07-05 21:55:06 +0200

    Add private actor recipient space

---
+++ src/app/espace-prive/acteur-prive/page.tsx
@@ -0,0 +1,5 @@
+import { ActorCoordinationInbox } from "@/components/private-space/ActorCoordinationInbox";
+
+export default function ActeurPrivePage() {
+  return <ActorCoordinationInbox actor="prive" />;
+}
