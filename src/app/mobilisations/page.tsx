3a172db49f929a6380d233f88d09ea31d0992561
mrdiamalick-maker <mrdiamalick@gmail.com>
2026-07-20 20:02:45 +0200

    Exposer la page publique Mobilisations

---
+++ src/app/mobilisations/page.tsx
@@ -0,0 +1,5 @@
+import { PublicMobilizationsExperience } from "@/components/public/PublicMobilizationsExperience";
+
+export default function MobilizationsPage() {
+  return <PublicMobilizationsExperience />;
+}
